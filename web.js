c34bf9ca28643a7e594e74f38413072e1dc6a06b
jimwinquist <jawinquist@gmail.com>
2013-07-14 15:10:55 +0000

    Updated web.js to properly update index.html when changes are made.

--- web.js
+++ web.js
@@ -1,15 +1,12 @@
 var express = require('express');
-
 var fs = require('fs');
-
-var indexBuffer = fs.readFileSync('index.html');
-
-var indexString = indexBuffer.toString('utf-8');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(indexString);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
 var port = process.env.PORT || 8080;
